95f9745a523bca0e19554bfefb5e85c9773e9e03
333623866 <333623866@C-JWZ7KS1.tdsb.on.ca>
2019-09-19 13:40:34 -0400

    :)

---
+++ AboutMe.java
@@ -0,0 +1,24 @@
+package pan.unit1;
+
+import java.util.Scanner;
+
+
+/** All About Me, First Assignment, Get to know Java
+ * September 17, 2018 
+ * @authorLesley
+ */
+
+public class AboutMe {
+	/** 
+	 * This is where our program starts.
+	 * @param args unused
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Lesley P.");
+		System.out.println("Mrs. Kemp");
+		System.out.println("\"13-when, 13-10!!\"");
+	}
+}
+
+
