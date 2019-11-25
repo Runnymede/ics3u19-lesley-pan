9aaeaaa57fbba3154d3fef85d0920f96ada895e9
333623866 <333623866@C-JWZ7KS1.tdsb.on.ca>
2019-11-25 13:20:22 -0500

    updated bored

--- ICS3U - Lesley/src/pan/unit1/Bored.java
+++ ICS3U - Lesley/src/pan/unit1/Bored.java
@@ -5,32 +5,53 @@ import java.util.Scanner;
 /** Just Doodles of Bored
  * 
  *Started: Sept 19, 2019
- *Last Updated: Sept 19
+ *Last Updated: Oct 2
  *@authorLesley
  */
 
-public class Bored {
+public class Bored
+{
 	
-	/* This is the first stage */
+	/* Interactive Story */
 	
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException 
+	{
+		/** 
+		 * This is where the fun begins.
+		 * @param args unused
+		 */
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Hello World!");
-		System.out.println("Isn't it a beautiful day?");
-		Thread.sleep(2000);
-		System.out.println();
+		//Guide welcomes user to world and asks for name//
+		
+		System.out.print("Hi, my name is I/O. What's your name?");
+		String name = sc.next();
+		System.out.print(name + ".. how interesting...");
+		Thread.sleep(3000);
+		System.out.println("\n \n \n \n \n \n");
+		
+		//Guide's Introduction to World//
+
+		
+		//
+		System.out.println("\"Hello World!\"");
+		System.out.println("\"Isn't it a beautiful day?\"");
+		System.out.println("It seems like this being wants you to respond. Yes/No (Beware they're sensitive to case)");
+		String beginResponse = sc.next();
+		
+		if (beginResponse == "Yes")
+		//	
+
+		
+	
+
+		//System.out.println();
 		System.out.println("\"is it really?\"");
 		Thread.sleep(3000);
-		System.out.println("\n");
-		System.out.println("\n");
-		System.out.println("\n");
-		System.out.println("\n");
-		System.out.println("\n");
+		System.out.println("\n \n \n");
 		System.out.println("\"hi my name is georgie\"");
-		System.out.println("\n");
-		System.out.println("\n");
-		System.out.println("\n");
-		System.out.println("\n");
+		System.out.println("\n \n \n \n \n");//
+	
 	
 		
 
@@ -41,7 +62,7 @@ public class Bored {
 			
 		
 
-	}
+}
 
 
 
